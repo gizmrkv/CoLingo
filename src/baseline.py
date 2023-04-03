f1e067eeea66e1b27864b96da3fd0cbfe17205f9
gizmrkv <gizmrkv@gmail.com>
2023-04-04 01:03:44 +0900

    Add baseline

---
+++ src/baseline.py
@@ -0,0 +1,30 @@
+import torch as th
+from abc import ABC, abstractmethod
+
+
+class Baseline(ABC):
+    @abstractmethod
+    def __call__(self, state: th.Tensor) -> th.Tensor:
+        pass
+
+    @abstractmethod
+    def update(self, state: th.Tensor, reward: th.Tensor):
+        pass
+
+
+class MeanBaseline(Baseline):
+    def __init__(self) -> None:
+        self.mean = th.zeros(1, requires_grad=False)
+        self.count = 0
+
+    def __call__(self, state: th.Tensor) -> th.Tensor:
+        return self.mean
+
+    def update(self, state: th.Tensor, reward: th.Tensor):
+        self.count += 1
+        self.mean += (reward.detach().mean().item() - self.mean) / self.count
+
+
+def build_baseline(baseline_type: str, baseline_args: dict) -> Baseline:
+    baselines_dict = {"mean": MeanBaseline}
+    return baselines_dict[baseline_type](**baseline_args)
