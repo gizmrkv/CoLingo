44e5b7e4b509191c5af1f9f88d212bfc852769a1
gizmrkv <gizmrkv@gmail.com>
2023-04-05 18:19:09 +0900

    Update: Abstract Network class

--- src/network.py
+++ src/network.py
@@ -1,8 +1,16 @@
-class CustomNetwork:
-    def __init__(self, edges: list, shuffle=True):
+from abc import ABC
+
+
+class Network(ABC):
+    def __init__(self, edges: list[dict[str, str]]):
         self.edges = edges
 
 
-def build_network(network_type: str, network_args: dict):
+class CustomNetwork(Network):
+    def __init__(self, edges: list[dict[str, str]]):
+        super().__init__(edges)
+
+
+def build_network(network_type: str, network_args: dict) -> Network:
     networks_dict = {"custom": CustomNetwork}
     return networks_dict[network_type](**network_args)
