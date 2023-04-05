047908b4cc951dfbb92fd8895a56bf7405c94528
gizmrkv <gizmrkv@gmail.com>
2023-04-05 18:20:15 +0900

    Specify: Return type for build_dataloader function

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,9 @@
 import torch as th
+from torch.utils.data import DataLoader
 
 
-def build_dataloader(dataloader_type: str, dataset: th.Tensor, dataloader_args: dict):
-    dataloaders_dict = {"default": th.utils.data.DataLoader}
+def build_dataloader(
+    dataloader_type: str, dataset: th.Tensor, dataloader_args: dict
+) -> DataLoader:
+    dataloaders_dict = {"default": DataLoader}
     return dataloaders_dict[dataloader_type](dataset, **dataloader_args)
