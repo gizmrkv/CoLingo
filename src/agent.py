d22210ce7d331c4336d74383168bdf8bf1f04a69
gizmrkv <gizmrkv@gmail.com>
2023-04-12 17:18:07 +0900

    Update Agent to initialize Model parameters

--- src/agent.py
+++ src/agent.py
@@ -20,20 +20,23 @@ class Agent(th.nn.Module):
     ):
         super().__init__()
         self.model = copy.deepcopy(model)
+        for param in self.model.parameters():
+            th.nn.init.normal_(param)
+
         self.optimizer = build_optimizer_type(optimizer_type)(
             self.model.parameters(), **optimizer_args
         )
         self.baseline = build_baseline(baseline_type, baseline_args)
 
-    def forward(self, x: th.Tensor, input_type: str, is_training: bool = False):
-        self.x, self.log_prob, self.entropy = self.model(x, input_type, is_training)
+    def forward(self, x: th.Tensor, input_type: str):
+        self.x, self.log_prob, self.entropy = self.model(x, input_type)
         return self.x
 
     def loss(self, reward: th.Tensor):
         self.baseline.update(self.x, reward)
-        if self.log_prob:
-            return -self.log_prob * (
-                reward - self.baseline(self.x) - 0.0 * self.entropy
+        if self.log_prob is not None:
+            return -self.log_prob.mean() * (
+                reward - self.baseline(self.x) - 0.0 * self.entropy.mean()
             )
         else:
             return -reward
