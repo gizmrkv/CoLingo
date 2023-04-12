79fc30898b5d060918a9346f75be71754d3402b7
gizmrkv <gizmrkv@gmail.com>
2023-04-12 17:18:35 +0900

    Add SequenceModel test

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
 import toml
 
+from torch.utils.data import DataLoader
+
 from src.model import build_model
-from src.network import build_network
+from src.network import build_network, Network
 from src.dataset import build_dataset
 from src.dataloader import build_dataloader
-from src.game import build_game
+from src.game import build_game, Game
 from src.agent import Agent
 from src.trainer import Trainer
 
 
+class ValidationGame(Game):
+    def __init__(
+        self,
+        agents: dict[str, Agent],
+        network: Network,
+        dataloader: DataLoader,
+        play_rate: float,
+    ):
+        super().__init__(agents, network, dataloader, play_rate)
+        self.dataset = dataloader.dataset
+        self.count = 0
+
+    def play(self):
+        sender = self.agents["S01"]
+        receiver1 = self.agents["R01"]
+
+        for agent in [sender, receiver1]:
+            agent.eval()
+
+        message = sender(self.dataset, "object")
+        answer1 = receiver1(message, "message")
+
+        # object = self.dataset.argmax(dim=-1)
+        # message = message.argmax(dim=-1)
+        answer1 = answer1.argmax(dim=-1)
+
+        print(f"{message} -> {answer1}")
+
+
+def fix_seed(seed: int):
+    import random
+    import numpy as np
+    import torch
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
 def main(config: dict):
+    fix_seed(config["training"]["seed"])
+
     datasets = {
         dataset_name: build_dataset(dataset_info["type"], dataset_info["args"])
         for dataset_name, dataset_info in config["datasets"].items()
@@ -39,26 +83,31 @@ def main(config: dict):
         for agent_name, agent_info in config["agents"].items()
     }
 
+    for agent in agents.values():
+        agent.train()
+
     games = {
         game_name: build_game(
             game_info["type"],
             agents,
             networks[game_info["network"]],
             dataloaders[game_info["dataloader"]],
+            game_info["play_rate"],
             game_info["args"],
         )
         for game_name, game_info in config["games"].items()
     }
 
-    trainer = Trainer(games)
-    trainer.train(config["training"]["n_epochs"], 1000)
+    validation = ValidationGame(
+        agents,
+        network=networks["custom_net1"],
+        dataloader=dataloaders["onehots_loader1"],
+        play_rate=0.001,
+    )
+    games["validation"] = validation
 
-    message = agents["smith"](datasets["onehots1"], "object", is_training=False)
-    answer = agents["brown"](message, "message", is_training=False)
-    object = datasets["onehots1"].argmax(dim=-1)
-    message = message.argmax(dim=-1)
-    answer = answer.argmax(dim=-1)
-    print(f"{object} -> {message} -> {answer}")
+    trainer = Trainer(games)
+    trainer.train(config["training"]["n_epochs"])
 
 
 if __name__ == "__main__":
