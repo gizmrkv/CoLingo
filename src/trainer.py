7b63b6d6153ed71469ff1c60a003335534c11d4d
gizmrkv <gizmrkv@gmail.com>
2023-04-12 17:16:25 +0900

    Update Trainer to adjust Game play frequency

--- src/trainer.py
+++ src/trainer.py
@@ -1,10 +1,14 @@
+from .game import Game
+import math
+
+
 class Trainer:
-    def __init__(self, games):
+    def __init__(self, games: dict[str, Game]):
         self.games = games
 
-    def train(self, n_epochs: int, print_interval: int = 100):
+    def train(self, n_epochs: int):
         for epoch in range(n_epochs):
-            reward = self.games["lewis1"].play()
-
-            if epoch % print_interval == 0:
-                print(f"epoch: {epoch}, reward: {reward:.6f}")
+            for game_name, game in self.games.items():
+                game.play_pool, play_count = math.modf(game.play_pool + game.play_rate)
+                for _ in range(int(play_count)):
+                    game.play()
