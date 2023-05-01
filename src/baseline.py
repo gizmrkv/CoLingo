a91f7a5d76ffd1a947ba3595694a7c15f80dd4de
gizmrkv <gizmrkv@gmail.com>
2023-05-01 22:42:24 +0900

    Update baseline from ABC to th.nn.Module

--- src/baseline.py
+++ src/baseline.py
@@ -1,25 +1,22 @@
 import torch as th
-from abc import ABC, abstractmethod
 
 
-class Baseline(ABC):
-    @abstractmethod
-    def __call__(self, state: th.Tensor) -> th.Tensor:
-        raise NotImplementedError
-
-    @abstractmethod
-    def update(self, state: th.Tensor, reward: th.Tensor):
-        raise NotImplementedError
-
-
-class MeanBaseline(Baseline):
+class MeanBaseline(th.nn.Module):
     def __init__(self) -> None:
+        super().__init__()
         self.mean = th.zeros(1, requires_grad=False)
         self.count = 0
 
-    def __call__(self, state: th.Tensor) -> th.Tensor:
+    def forward(self, loss: th.Tensor) -> th.Tensor:
+        if self.training:
+            self.count += 1
+            self.mean += (loss.detach().mean().item() - self.mean) / self.count
         return self.mean
 
-    def update(self, state: th.Tensor, reward: th.Tensor):
-        self.count += 1
-        self.mean += (reward.detach().mean().item() - self.mean) / self.count
+
+class BatchMeanBaseline(th.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, loss: th.Tensor) -> th.Tensor:
+        return loss.mean(dim=0)
