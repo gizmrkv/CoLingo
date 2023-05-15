ad630a4b11847078968a4b4bbe665d8c12452feb
gizmrkv <gizmrkv@gmail.com>
2023-05-15 17:13:57 +0900

    Fix baseline

--- src/core/baseline.py
+++ src/core/baseline.py
@@ -4,14 +4,14 @@ import torch as th
 class MeanBaseline(th.nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.mean = th.nn.Parameter(th.zeros(1), requires_grad=False)
-        self.count = 0
+        self._mean = th.nn.Parameter(th.zeros(1), requires_grad=False)
+        self._count = 0
 
     def forward(self, loss: th.Tensor) -> th.Tensor:
         if self.training:
-            self.count += 1
-            self.mean += (loss.mean().item() - self.mean) / self.count
-        return self.mean
+            self._count += 1
+            self._mean += (loss.mean().item() - self._mean) / self._count
+        return self._mean
 
 
 class BatchMeanBaseline(th.nn.Module):
