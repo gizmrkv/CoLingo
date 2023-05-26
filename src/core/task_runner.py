f588457f5eae3c02930a8eb07a67452d6d7f216c
gizmrkv <gizmrkv@gmail.com>
2023-05-26 12:36:17 +0900

    Update TaskRunner to print iter

--- src/core/task_runner.py
+++ src/core/task_runner.py
@@ -5,7 +5,7 @@ from .callback import Callback
 
 class TaskRunner:
     """
-    This class manages and executes a series of tasks represented as callbacks. 
+    This class manages and executes a series of tasks represented as callbacks.
     It provides an interface to run a certain number of iterations over these tasks.
 
     Args:
@@ -14,13 +14,14 @@ class TaskRunner:
     Methods:
         run(n_iterations: int): Runs the specified number of iterations over the tasks.
     """
+
     def __init__(self, tasks: Iterable[Callback]):
         self.tasks = tasks
 
     def run(self, n_iterations: int):
         """
-        Runs the specified number of iterations over the tasks. During each iteration, 
-        each task is updated. At the beginning and end of the run, the `on_begin` and 
+        Runs the specified number of iterations over the tasks. During each iteration,
+        each task is updated. At the beginning and end of the run, the `on_begin` and
         `on_end` methods of each task are called, respectively.
 
         Args:
@@ -29,7 +30,9 @@ class TaskRunner:
         for task in self.tasks:
             task.on_begin()
 
-        for _ in range(n_iterations):
+        for i in range(n_iterations):
+            print(f"Iteration {i + 1}/{n_iterations}")
+
             for task in self.tasks:
                 task.on_update()
 
