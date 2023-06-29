9b8ed99882786362649ee1ef96122fc94714f86b
gizmrkv <gizmrkv@gmail.com>
2023-06-29 18:01:21 +0900

    Update InferringGame

--- src/game/inferring.py
+++ src/game/inferring.py
@@ -1,9 +1,10 @@
 import random
 from dataclasses import dataclass
 from itertools import islice
-from typing import Any, Callable, Iterable, Tuple
+from typing import Any, Callable, Iterable
 
-import torch as th
+import torch
+from torch import nn, optim
 from torch.utils.data import DataLoader
 
 from ..core import Callback
@@ -12,108 +13,83 @@ from ..logger import Logger
 
 @dataclass
 class InferringGameResult:
-    input: th.Tensor
-    output: th.Tensor
-    target: th.Tensor
-    loss: th.Tensor | None = None
+    input: Any
+    output: Any
+    info: Any
 
 
-class InferringGame(th.nn.Module):
-    def __init__(self):
+class InferringGame(nn.Module):
+    def __init__(self, input_command: str = "input", output_command: str = "output"):
         super().__init__()
+        self.input_command = input_command
+        self.output_command = output_command
 
-    def forward(
-        self,
-        agent: th.nn.Module,
-        input: th.Tensor,
-        target: th.Tensor,
-        loss: th.nn.Module | None = None,
-        input_command: str = "input",
-        output_command: str = "output",
-    ):
-        hidden = agent(input=input, command=input_command)
-        output = agent(hidden=hidden, command=output_command)
-        result = InferringGameResult(input=input, output=output, target=target)
-        if self.training:
-            result.loss = loss(output, target)
-
+    def forward(self, agent, input) -> InferringGameResult:
+        latent = agent(input=input, command=self.input_command)
+        output, info = agent(latent=latent, command=self.output_command)
+        result = InferringGameResult(input=input, output=output, info=info)
         return result
 
 
 class InferringGameTrainer(Callback):
     def __init__(
         self,
-        agents: dict[str, th.nn.Module],
-        optimizers: dict[str, th.optim.Optimizer],
+        game: InferringGame,
+        agents: dict[str, nn.Module],
+        optimizers: dict[str, optim.Optimizer],
         dataloader: DataLoader,
-        loss: th.nn.Module,
+        loss: Callable[[InferringGameResult, torch.Tensor], torch.Tensor],
         max_batches: int = 1,
-        input_command: str = "input",
-        output_command: str = "output",
     ):
         super().__init__()
+        self.game = game
         self.agents = agents
         self.optimizers = optimizers
         self.dataloader = dataloader
         self.loss = loss
         self.max_batches = max_batches
-        self.input_command = input_command
-        self.output_command = output_command
-
-        self.game = InferringGame()
-        self.game.train()
 
         self.agent_names = list(agents.keys())
 
     def on_update(self, iteration: int):
         agent_name = random.choice(self.agent_names)
         agent = self.agents[agent_name]
-        optimizer = self.optimizers[agent_name]
+        optimizer = self.optimizers.get(agent_name)
+
+        if optimizer is None:
+            return
 
         agent.train()
         optimizer.zero_grad()
 
         for input, target in islice(self.dataloader, self.max_batches):
-            result: InferringGameResult = self.game(
-                agent=agent,
-                input=input,
-                target=target,
-                loss=self.loss,
-                input_command=self.input_command,
-                output_command=self.output_command,
-            )
-            result.loss.sum().backward(retain_graph=True)
+            result = self.game(agent=agent, input=input)
+            loss: torch.Tensor = self.loss(result, target)
+            loss.sum().backward(retain_graph=True)
             optimizer.step()
 
 
 class InferringGameEvaluator(Callback):
     def __init__(
         self,
-        agents: dict[str, th.nn.Module],
-        input: th.Tensor,
-        target: th.Tensor,
+        game: InferringGame,
+        agents: dict[str, nn.Module],
+        input: torch.Tensor,
         metric: Callable[[InferringGameResult], dict],
         logger: Logger | Iterable[Logger],
         name: str,
         run_on_begin: bool = True,
         run_on_end: bool = True,
-        input_command: str = "input",
-        output_command: str = "output",
     ):
         super().__init__()
+        self.game = game
         self.agents = agents
         self.input = input
-        self.target = target
         self.metric = metric
         self.loggers = [logger] if isinstance(logger, Logger) else logger
         self.name = name
         self.run_on_begin = run_on_begin
         self.run_on_end = run_on_end
-        self.input_command = input_command
-        self.output_command = output_command
-
-        self.game = InferringGame()
-        self.game.eval()
 
     def on_begin(self):
         if self.run_on_begin:
@@ -131,14 +107,8 @@ class InferringGameEvaluator(Callback):
         for agent_name, agent in self.agents.items():
             agent.eval()
 
-            with th.no_grad():
-                result: InferringGameResult = self.game(
-                    agent=agent,
-                    input=self.input,
-                    target=self.target,
-                    input_command=self.input_command,
-                    output_command=self.output_command,
-                )
+            with torch.no_grad():
+                result = self.game(agent=agent, input=self.input)
 
             metric = self.metric(result)
             log |= {agent_name: metric}
