653aae229a1e2e45f0768de3224d50f2e20ba3c3
gizmrkv <gizmrkv@gmail.com>
2023-06-29 18:01:02 +0900

    Fix WandBLogger initialize params

--- src/logger/wandb.py
+++ src/logger/wandb.py
@@ -4,9 +4,9 @@ from .logger import Logger
 
 
 class WandBLogger(Logger):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__()
-        wandb.init(**kwargs)
+        wandb.init(*args, **kwargs)
 
     def flush(self):
         wandb.log(self.logs)
