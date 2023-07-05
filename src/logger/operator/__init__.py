7dd22e3696de871a935791014a98f019496e771e
gizmrkv <gizmrkv@gmail.com>
2023-07-05 22:55:03 +0900

    Add take, skip, take_while, skip_while

--- src/logger/operator/__init__.py
+++ src/logger/operator/__init__.py
@@ -1,6 +1,7 @@
 from .count import count
 from .filter import filter
 from .fold import fold, max, min, product, sum
+from .fold_filter import fold_filter, skip, skip_while, take, take_while
 from .inspect import inspect
 from .once import once
 from .pipe import pipe
