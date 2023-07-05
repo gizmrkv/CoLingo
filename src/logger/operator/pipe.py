254664daf913ed0e22fbd5a4e2123df22e082470
gizmrkv <gizmrkv@gmail.com>
2023-07-05 22:21:17 +0900

    Update pipe to be able to pipe pipes

--- src/logger/operator/pipe.py
+++ src/logger/operator/pipe.py
@@ -1,5 +1,3 @@
-from typing import Iterable
-
 from ..logger import Logger
 from .logger_operator import LoggerOperator
 
@@ -7,6 +5,7 @@ from .logger_operator import LoggerOperator
 class PipeLoggerOperator(LoggerOperator):
     def __init__(self, *loggers: Logger):
         super().__init__(*loggers)
+        self._tail = loggers[-1]
 
         for i in range(len(loggers) - 1):
             curr, next = loggers[i : i + 2]
@@ -15,6 +14,9 @@ class PipeLoggerOperator(LoggerOperator):
 
     def log(self, *args, **kwargs):
         self._loggers[0].log(*args, **kwargs)
+
+    def subscribe(self, logger: Logger):
+        self._tail.subscribe(logger)
 
 
 def pipe(*loggers: Logger):
