55988f77c9678483d2bd33914f471af49a11596d
gizmrkv <gizmrkv@gmail.com>
2023-07-09 14:00:53 +0900

    Update sweep to can resume

--- sweep.py
+++ sweep.py
@@ -4,20 +4,28 @@ import toml
 import yaml
 
 import wandb
-from src.experiment import run_signaling
+from src.experiment import run_disc_seq_mlp_exp, run_disc_seq_rnn_exp
 
 
-def sweep_monologue():
+def sweep():
     wandb.init()
     config = dict(wandb.config)
-    run_signaling(config)
+    run_disc_seq_mlp_exp(config)
 
+
+import argparse
 
 if __name__ == "__main__":
-    # sweep_path = "config_sweep/monologue_hyperparam_search.toml"
-    # sweep_path = "config_sweep/length_penalty_search.toml"
-    # sweep_path = "config_sweep/sender_output_search.toml"
-    sweep_path = "config_sweep/loss_scaling_search.toml"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_path", type=str, help="Path to config file")
+    parser.add_argument("--id", type=str, help="Sweep ID")
+    parser.add_argument(
+        "--wandb_project", "-p", type=str, help="Wandb project name", default="CoLingo"
+    )
+
+    args = parser.parse_args()
+
+    sweep_path = args.config_path
     with open(sweep_path, "r") as f:
         if sweep_path.endswith(".json"):
             sweep_config = json.load(f)
@@ -28,5 +36,5 @@ if __name__ == "__main__":
         else:
             raise ValueError(f"Unknown file extension: {sweep_path}")
 
-    sweep_id = wandb.sweep(sweep=sweep_config)
-    wandb.agent(sweep_id, function=sweep_monologue)
+    sweep_id = args.id if args.id else wandb.sweep(sweep=sweep_config)
+    wandb.agent(sweep_id, function=sweep, project=args.wandb_project)
